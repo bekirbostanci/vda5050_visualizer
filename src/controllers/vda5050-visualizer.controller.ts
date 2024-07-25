5327950fb5bd27ca5bacbb3fea68db55d2b832ce
bekirbostanci <bekirbostanci@gmail.com>
2024-07-25 22:03:07 +0200

    fix: track robot with all message not just connection messag e

--- src/controllers/vda5050-visualizer.controller.ts
+++ src/controllers/vda5050-visualizer.controller.ts
@@ -1,4 +1,7 @@
-import { getMasterController } from "@/controllers/vda5050.controller";
+import {
+  getMasterController,
+  getMqttClient,
+} from "@/controllers/vda5050.controller";
 import type { AgvId, MasterController } from "vda-5050-lib";
 import { ref } from "vue";
 export class VDA5050Visualizer {
@@ -6,15 +9,33 @@ export class VDA5050Visualizer {
   public mc: MasterController | undefined;
   constructor() {
     this.mc = getMasterController();
-    this.mc.trackAgvs((agvId: AgvId) => {
-      if (
-        this.robotList.value?.find(
-          (agv) =>
-            agv.serialNumber == agvId.serialNumber &&
-            agv.manufacturer == agvId.manufacturer
-        ) == undefined
-      ) {
-        this.robotList.value?.push(agvId);
+
+    const mqttTopic =
+      this.mc.clientOptions.interfaceName +
+      "/v" +
+      this.mc.clientOptions.vdaVersion.substring(0, 1);
+
+    const mqttClient = getMqttClient();
+    mqttClient.subscribe(mqttTopic + "/#");
+
+    mqttClient.on("message", (topic: string, message) => {
+      if (topic.indexOf(mqttTopic) != -1) {
+        const topic_split = topic.split("/");
+
+        const agvId = {
+          manufacturer: topic_split[2],
+          serialNumber: topic_split[3],
+        };
+
+        if (
+          this.robotList.value?.find(
+            (agv) =>
+              agv.serialNumber == agvId.serialNumber &&
+              agv.manufacturer == agvId.manufacturer
+          ) == undefined
+        ) {
+          this.robotList.value?.push(agvId);
+        }
       }
     });
   }
