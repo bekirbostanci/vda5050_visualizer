d3d8ed4cbcccfcf478dbfc8d2a37387a02b672c2
bekirbostanci <bekirbostanci@gmail.com>
2026-02-04 22:17:08 +0100

    feat:  hide for menu bar in main window

--- electron/main.js
+++ electron/main.js
@@ -58,6 +58,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
